14b22971b89db244359151670c221e0376e282d2
Jerry Ding <13386637338@qq.com>
2024-02-11 18:08:18 -0500

    ex env

--- scripts/robust_ds.sh
+++ scripts/robust_ds.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 set -x
+
+module load anaconda3/2022.05 cuda/11.1 gcc/10.1.0
+source activate sense_release
+
 CUDA_VISIBLE_DEVICES=0,1,2,3 python ds_robust.py --dataset drivingstereo \
     --maxdisp 256 \
     --epochs 10 --lr 0.001  --lrepochs "5:10" \
