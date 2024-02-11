7442411f010ae736548eaf274ccb45e8bd80f5d0
Jerry Ding <13386637338@qq.com>
2024-02-11 18:06:45 -0500

    ds script fix

--- scripts/robust_ds.sh
+++ scripts/robust_ds.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -x
-CUDA_VISIBLE_DEVICES=0,1,2,3 python robust.py --dataset drivingstereo \
+CUDA_VISIBLE_DEVICES=0,1,2,3 python ds_robust.py --dataset drivingstereo \
     --maxdisp 256 \
     --epochs 10 --lr 0.001  --lrepochs "5:10" \
     --loadckpt "/work/vig/tianyed/CFNet/CFNet/pretrained_checkpoints/sceneflow_pretraining.ckpt" \
