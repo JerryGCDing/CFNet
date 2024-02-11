4bdc5a752781f05a40024b7d7559a40459122824
Jerry Ding <13386637338@qq.com>
2024-02-11 18:13:50 -0500

    script fix

--- scripts/robust_ds.sh
+++ scripts/robust_ds.sh
@@ -4,7 +4,7 @@ set -x
 module load anaconda3/2022.05 cuda/11.1 gcc/10.1.0
 source activate sense_release
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 python ds_robust.py --dataset drivingstereo \
+CUDA_VISIBLE_DEVICES=0,1,2,3 python /work/vig/tianyed/CFNet/CFNet/ds_robust.py --dataset 'drivingstereo' \
     --maxdisp 256 \
     --epochs 10 --lr 0.001  --lrepochs "5:10" \
     --loadckpt "/work/vig/tianyed/CFNet/CFNet/pretrained_checkpoints/sceneflow_pretraining.ckpt" \
